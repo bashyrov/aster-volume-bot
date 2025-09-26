9360808c149dfa40cbcabdb97fbb2e59cdac34f3
bashyrov <bashyroov@gmail.com>
2025-09-26 12:09:35 +0200

    add wallet field to Api_Keys model

--- origin/models.py
+++ origin/models.py
@@ -11,18 +11,19 @@ class UserProfile(models.Model):
         return self.tg_id
 
 
-class Api_Keys(models.Model):
-    key = models.CharField(max_length=120)
-    sec_key = models.CharField(max_length=120)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='api_keys')
-
-    def __str__(self):
-        return f"{self.key} - {self.user}"
-
-
 class Wallet(models.Model):
     wallet = models.CharField(max_length=255, blank=False, null=False)
     balance = models.IntegerField(blank=False, null=False)
 
     def __str__(self):
         return f"{self.wallet} - {self.balance}"
+
+
+class Api_Keys(models.Model):
+    key = models.CharField(max_length=120)
+    sec_key = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='api_keys')
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='api_keys')
+
+    def __str__(self):
+        return f"{self.key} - {self.user}"
