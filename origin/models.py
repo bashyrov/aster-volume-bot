4053244c53cd23327b776d268c5625eef8b11ed2
bashyrov <bashyroov@gmail.com>
2025-09-26 12:06:41 +0200

    create base models - toke, user, Api Keys

---
+++ origin/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    tg_id = models.CharField(max_length=50, blank=False, null=False)
+    token = models.CharField(max_length=50, blank=False, null=False)
+    leverage = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.tg_id
+
+
+class Api_Keys(models.Model):
+    key = models.CharField(max_length=120)
+    sec_key = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='api_keys')
+
+    def __str__(self):
+        return f"{self.key} - {self.user}"
+
+
+class Wallet(models.Model):
+    wallet = models.CharField(max_length=255, blank=False, null=False)
+    balance = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.wallet} - {self.balance}"
